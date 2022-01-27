3d084416efdc589cf2bf1a859b801caac55e976d
ValdesRiveraDaniel <73697457+ValdesRiveraDaniel@users.noreply.github.com>
2022-01-27 20:11:18 +0100

    commit Final
    aplicación terminada

---
+++ UD5_01_App.net/registroCoche/registroCoche/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace registroCoche
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Añadir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
+            this.editar = new System.Windows.Forms.Button();
+            this.borrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.borrar);
+            this.splitContainer1.Panel1.Controls.Add(this.editar);
+            this.splitContainer1.Panel1.Controls.Add(this.Añadir);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(986, 572);
+            this.splitContainer1.SplitterDistance = 199;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Añadir
+            // 
+            this.Añadir.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Añadir.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Añadir.Location = new System.Drawing.Point(12, 12);
+            this.Añadir.Name = "Añadir";
+            this.Añadir.Size = new System.Drawing.Size(175, 23);
+            this.Añadir.TabIndex = 0;
+            this.Añadir.Text = "Añadir\r\n\r\n\r\n\r\n\r\n";
+            this.Añadir.UseVisualStyleBackColor = false;
+            this.Añadir.Click += new System.EventHandler(this.Añadir_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(783, 572);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
+            // editar
+            // 
+            this.editar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.editar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.editar.Location = new System.Drawing.Point(12, 66);
+            this.editar.Name = "editar";
+            this.editar.Size = new System.Drawing.Size(175, 23);
+            this.editar.TabIndex = 1;
+            this.editar.Text = "Editar";
+            this.editar.UseVisualStyleBackColor = false;
+            this.editar.Click += new System.EventHandler(this.editar_Click);
+            // 
+            // borrar
+            // 
+            this.borrar.BackColor = System.Drawing.Color.Red;
+            this.borrar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.borrar.Location = new System.Drawing.Point(12, 120);
+            this.borrar.Name = "borrar";
+            this.borrar.Size = new System.Drawing.Size(175, 23);
+            this.borrar.TabIndex = 2;
+            this.borrar.Text = "Borrar";
+            this.borrar.UseVisualStyleBackColor = false;
+            this.borrar.Click += new System.EventHandler(this.borrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 572);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Añadir;
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
+        private System.Windows.Forms.Button editar;
+        private System.Windows.Forms.Button borrar;
+    }
+}
+
---
+++ UD5_01_App.net/registroCoche/registroCoche/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using registroCoche.modelos;
+
+namespace registroCoche
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            refrescar();
+        }
+
+        private void refrescar() {
+
+            using (aplicacion1Entities DB = new aplicacion1Entities())
+            {
+                var lst = from d in DB.Coches select d;
+
+                dataGridView1.DataSource = lst.ToList();
+            }
+
+        }
+
+        private void Añadir_Click(object sender, EventArgs e)
+        {
+            ventanas.frmAñadir ofrmAñadir = new ventanas.frmAñadir();
+            ofrmAñadir.ShowDialog();
+
+            refrescar();
+        }
+
+        private int? getID() {
+            try { 
+                return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch {
+                return null;
+            }
+        }
+
+        private void editar_Click(object sender, EventArgs e)
+        {
+            int? id = getID();
+            if (id != null) {
+                ventanas.frmAñadir oFrmEditar = new ventanas.frmAñadir(id);
+                oFrmEditar.ShowDialog();
+                refrescar();
+            }
+        }
+
+        private void borrar_Click(object sender, EventArgs e)
+        {
+            int? id = getID();
+            if (id != null)
+            {
+                using (aplicacion1Entities DB = new aplicacion1Entities()) {
+                    Coches oCoches = DB.Coches.Find(id);
+                    DB.Coches.Remove(oCoches);
+                    DB.SaveChanges();
+                }
+                refrescar();
+            }
+        }
+    }
+}
